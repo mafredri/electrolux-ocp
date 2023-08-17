70fd53c247fba3fec00c1282973fb5539e5f3fbb
Mathias Fredriksson <mafredri@gmail.com>
2023-08-17 21:41:03 +0300

    Refactor, fix refresh token

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/mafredri/electrolux-ocp
 
 go 1.20
 
-require golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b
+require golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
--- ocpapi/ocpapi.go
+++ ocpapi/ocpapi.go
@@ -18,23 +18,37 @@ const (
 	APIURL = "https://api.ocp.electrolux.one"
 )
 
+// State contains the current state of the client
+// (e.g. for saving and restoring auth tokens).
+type State struct {
+	RegionalBaseURL string `json:"regionalBaseUrl"`
+	ClientToken     Token  `json:"clientToken"`
+	UserToken       Token  `json:"userToken"`
+}
+
+// Client is an Electrolux OCP API client.
 type Client struct {
 	config Config
 	client *http.Client
 
-	regionalBaseURL string
-	authToken       AuthToken
+	state State
 }
 
 type Config struct {
+	APIURL       string
 	APIKey       string
 	Brand        string // Example: "electrolux"
 	ClientID     string // Example: "ElxOneApp"
 	ClientSecret string
 	CountryCode  string // Example: "FI"
+
+	State State // Optional initial state.
 }
 
 func New(config Config) (*Client, error) {
+	if config.APIURL == "" {
+		config.APIURL = APIURL
+	}
 	if config.APIKey == "" {
 		return nil, errors.New("missing APIKey")
 	}
@@ -57,29 +71,81 @@ func New(config Config) (*Client, error) {
 	c := &Client{
 		client: httpClient,
 		config: config,
+		state:  config.State,
 	}
 	httpClient.Transport = newClientTransport(http.DefaultTransport, config.APIKey)
 
 	return c, nil
 }
 
-func (c *Client) SetAuth(regionalBaseURL string, auth AuthToken) {
-	c.regionalBaseURL = regionalBaseURL
-	c.authToken = auth
+// State returns the current state of the client.
+func (c *Client) State() State {
+	return c.state
 }
 
-func (c *Client) GetAuth() (regionalBaseURL string, token AuthToken) {
-	return c.regionalBaseURL, c.authToken
+type IdentityProvider struct {
+	Domain                   string `json:"domain"` // "eu1.gigya.com"
+	APIKey                   string `json:"apiKey"`
+	Brand                    string `json:"brand"`                    // "electrolux"
+	HTTPRegionalBaseURL      string `json:"httpRegionalBaseUrl"`      // "https://api.eu.ocp.electrolux.one"
+	WebSocketRegionalBaseURL string `json:"webSocketRegionalBaseUrl"` // "wss://ws.eu.ocp.electrolux.one"
+	DataCenter               string `json:"dataCenter"`               // "EU"
+}
+
+func (c *Client) IdentityProviders(ctx context.Context, email string) ([]IdentityProvider, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/one-account-user/api/v1/identity-providers?brand=%s&email=%s", c.config.APIURL, c.config.Brand, email), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Context-Brand", c.config.Brand)
+
+	var ips []IdentityProvider
+	err = c.doClientAuth(ctx, req, &ips)
+	if err != nil {
+		return nil, fmt.Errorf("do client auth: %w", err)
+	}
+
+	return ips, nil
+}
+
+type Country struct {
+	Name           string `json:"name"`
+	CountryCode    string `json:"countryCode"`
+	LegalRegion    string `json:"legalRegion"`    // "APMEA", "EU (GDPR)", "LAM", "US"
+	BusinessRegion string `json:"businessRegion"` // "BA-APMEA", "BA-EU", "BA-LATAM", "BA-NA"
+	DataCenter     string `json:"dataCenter"`     // "AU", "US", "EU"
+}
+
+func countryCodes(cs ...Country) (codes []string) {
+	for _, c := range cs {
+		codes = append(codes, c.CountryCode)
+	}
+	return codes
+}
+
+func (c *Client) Countries(ctx context.Context) ([]Country, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/one-account-user/api/v1/countries", c.state.RegionalBaseURL), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var countries []Country
+	err = c.doClientAuth(ctx, req, &countries)
+	if err != nil {
+		return nil, fmt.Errorf("do client auth: %w", err)
+	}
+
+	return countries, nil
 }
 
 // Login logs in to the API using the provided email and password.
 func (c *Client) Login(ctx context.Context, email, password string) error {
-	token, err := c.ClientToken(ctx)
-	if err != nil {
-		return fmt.Errorf("client token: %w", err)
+	if c.state.RegionalBaseURL != "" && c.state.UserToken.RefreshToken != "" {
+		// Assume a valid base URL and token has been provided.
+		return nil
 	}
 
-	ips, err := c.identityProviders(ctx, token, email)
+	ips, err := c.IdentityProviders(ctx, email)
 	if err != nil {
 		return fmt.Errorf("identity providers: %w", err)
 	}
@@ -92,15 +158,20 @@ func (c *Client) Login(ctx context.Context, email, password string) error {
 		return fmt.Errorf("multiple identity providers found, only one is supported: found %d providers", len(ips))
 	}
 	ip := ips[0]
-	c.regionalBaseURL = ip.HTTPRegionalBaseURL
+	c.state.RegionalBaseURL = ip.HTTPRegionalBaseURL
 
-	cs, err := c.countries(ctx, token)
+	if c.state.UserToken.RefreshToken != "" {
+		// Assume a valid token has been provided.
+		return nil
+	}
+
+	countries, err := c.Countries(ctx)
 	if err != nil {
 		return fmt.Errorf("countries: %w", err)
 	}
 
-	if !slices.Contains(cs.CountryCodes(), c.config.CountryCode) {
-		return fmt.Errorf("country code %q not found in available countries: %v", c.config.CountryCode, cs.CountryCodes())
+	if codes := countryCodes(countries...); !slices.Contains(codes, c.config.CountryCode) {
+		return fmt.Errorf("country code %q not found in available countries: %v", c.config.CountryCode, codes)
 	}
 
 	gi := gigya.NewIdentity(gigya.Config{
@@ -112,7 +183,7 @@ func (c *Client) Login(ctx context.Context, email, password string) error {
 		return fmt.Errorf("gigya login: %w", err)
 	}
 
-	c.authToken, err = c.login(ctx, idToken)
+	c.state.UserToken, err = c.tokenExchange(ctx, idToken)
 	if err != nil {
 		return fmt.Errorf("auth token: %w", err)
 	}
@@ -120,7 +191,84 @@ func (c *Client) Login(ctx context.Context, email, password string) error {
 	return nil
 }
 
-func (c *Client) ClientToken(ctx context.Context) (ClientToken, error) {
+// Appliances contains data from all appliances.
+func (c *Client) Appliances(ctx context.Context, includeMetadata bool) ([]Appliance, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/appliance/api/v2/appliances?includeMetadata=%t", c.state.RegionalBaseURL, includeMetadata), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var appliances []Appliance
+	err = c.doUserAuth(ctx, req, &appliances)
+	if err != nil {
+		return nil, fmt.Errorf("do user auth: %w", err)
+	}
+
+	return appliances, nil
+}
+
+// AppliancesInfo contains information about the requested appliances.
+func (c *Client) AppliancesInfo(ctx context.Context, applianceIDs ...string) ([]ApplianceInfo, error) {
+	body, err := json.Marshal(map[string][]string{"applianceIds": applianceIDs})
+	if err != nil {
+		return nil, fmt.Errorf("marshal: %w", err)
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/appliance/api/v2/appliances/info", c.state.RegionalBaseURL), bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	var applianceInfo []ApplianceInfo
+	err = c.doUserAuth(ctx, req, &applianceInfo)
+	if err != nil {
+		return nil, fmt.Errorf("do user auth: %w", err)
+	}
+
+	return applianceInfo, nil
+}
+
+// Token is an access token generated via client or user authentication.
+type Token struct {
+	AccessToken  string    `json:"accessToken"`            // JWT.
+	ExpiresIn    int       `json:"expiresIn"`              // Seconds.
+	ExpiresAt    time.Time `json:"expiresAt"`              // Set by UnmarshalJSON (now + expires in).
+	TokenType    string    `json:"tokenType"`              // "Bearer"
+	RefreshToken string    `json:"refreshToken,omitempty"` // Set for login.
+	Scope        string    `json:"scope"`                  // Example: "", "email offline_access eluxiot:*:*:*"
+}
+
+// Authorization returns the Authorization header value for the token.
+func (t Token) Authorization() string {
+	return fmt.Sprintf("%s %s", t.TokenType, t.AccessToken)
+}
+
+// UnmarshalJSON implements json.Unmarshaler and assigns ExpiresAt.
+func (t *Token) UnmarshalJSON(b []byte) error {
+	now := time.Now()
+
+	type token Token
+	var tmp token
+	if err := json.Unmarshal(b, &tmp); err != nil {
+		return err
+	}
+	*t = Token(tmp)
+	if t.ExpiresAt.IsZero() {
+		t.ExpiresAt = now.Add(time.Duration(t.ExpiresIn) * time.Second)
+	}
+	return nil
+}
+
+type tokenRequest struct {
+	GrantType    string `json:"grantType"`
+	ClientID     string `json:"clientId"`
+	ClientSecret string `json:"clientSecret,omitempty"` // Set for ClientToken.
+	IDToken      string `json:"idToken,omitempty"`      // Set for login.
+	RefreshToken string `json:"refreshToken,omitempty"` // Set for refresh.
+	Scope        string `json:"scope"`
+}
+
+func (c *Client) clientCredentials(ctx context.Context) (Token, error) {
 	body, err := json.Marshal(tokenRequest{
 		GrantType:    "client_credentials",
 		ClientID:     c.config.ClientID,
@@ -128,30 +276,25 @@ func (c *Client) ClientToken(ctx context.Context) (ClientToken, error) {
 		Scope:        "",
 	})
 	if err != nil {
-		return ClientToken{}, fmt.Errorf("marshal: %w", err)
+		return Token{}, fmt.Errorf("marshal: %w", err)
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/one-account-authorization/api/v1/token", APIURL), bytes.NewReader(body))
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/one-account-authorization/api/v1/token", c.config.APIURL), bytes.NewReader(body))
 	if err != nil {
-		return ClientToken{}, fmt.Errorf("new request: %w", err)
+		return Token{}, fmt.Errorf("new request: %w", err)
 	}
+	req.Header.Add("Content-Type", "application/json")
 
-	resp, err := c.client.Do(req)
+	var t Token
+	err = c.do(ctx, req, &t)
 	if err != nil {
-		return ClientToken{}, fmt.Errorf("do: %w", err)
-	}
-	defer resp.Body.Close()
-
-	var t ClientToken
-	err = decodeResponse(resp, &t)
-	if err != nil {
-		return ClientToken{}, fmt.Errorf("decode response: %w", err)
+		return Token{}, fmt.Errorf("do: %w", err)
 	}
 
 	return t, nil
 }
 
-func (c *Client) login(ctx context.Context, idToken string) (AuthToken, error) {
+func (c *Client) tokenExchange(ctx context.Context, idToken string) (Token, error) {
 	return c.token(ctx, tokenRequest{
 		GrantType: "urn:ietf:params:oauth:grant-type:token-exchange",
 		ClientID:  c.config.ClientID,
@@ -160,7 +303,11 @@ func (c *Client) login(ctx context.Context, idToken string) (AuthToken, error) {
 	})
 }
 
-func (c *Client) refresh(ctx context.Context, token AuthToken) (AuthToken, error) {
+func (c *Client) refreshToken(ctx context.Context, token Token) (Token, error) {
+	if token.RefreshToken == "" {
+		return Token{}, errors.New("refresh token is missing")
+	}
+
 	return c.token(ctx, tokenRequest{
 		GrantType:    "refresh_token",
 		ClientID:     c.config.ClientID,
@@ -169,148 +316,78 @@ func (c *Client) refresh(ctx context.Context, token AuthToken) (AuthToken, error
 	})
 }
 
-func (c *Client) token(ctx context.Context, payload tokenRequest) (AuthToken, error) {
-	body, err := json.Marshal(payload)
+func (c *Client) token(ctx context.Context, tr tokenRequest) (Token, error) {
+	body, err := json.Marshal(tr)
 	if err != nil {
-		return AuthToken{}, fmt.Errorf("marshal: %w", err)
+		return Token{}, fmt.Errorf("marshal: %w", err)
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/one-account-authorization/api/v1/token", c.regionalBaseURL), bytes.NewReader(body))
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/one-account-authorization/api/v1/token", c.state.RegionalBaseURL), bytes.NewReader(body))
 	if err != nil {
-		return AuthToken{}, err
+		return Token{}, err
 	}
+	req.Header.Add("Authorization", "Bearer ")
+	req.Header.Add("Content-Type", "application/json")
 	req.Header.Set("Origin-Country-Code", c.config.CountryCode)
 
-	resp, err := c.client.Do(req)
+	var t Token
+	err = c.do(ctx, req, &t)
 	if err != nil {
-		return AuthToken{}, fmt.Errorf("do: %w", err)
-	}
-	defer resp.Body.Close()
-
-	var t AuthToken
-	err = decodeResponse(resp, &t)
-	if err != nil {
-		return AuthToken{}, err
+		return Token{}, fmt.Errorf("do: %w", err)
 	}
 
 	return t, nil
 }
 
-func (c *Client) identityProviders(ctx context.Context, token ClientToken, email string) ([]identityProvider, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/one-account-user/api/v1/identity-providers?brand=%s&email=%s", APIURL, c.config.Brand, email), nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("Authorization", token.Authorization())
-	req.Header.Add("Context-Brand", c.config.Brand)
-
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("do: %w", err)
-	}
-	defer resp.Body.Close()
-
-	var ips []identityProvider
-	err = decodeResponse(resp, &ips)
-	if err != nil {
-		return nil, err
-	}
-
-	return ips, nil
-}
-
-func (c *Client) countries(ctx context.Context, token ClientToken) (countries, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/one-account-user/api/v1/countries", c.regionalBaseURL), nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("Authorization", token.Authorization())
-
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("do: %w", err)
-	}
-	defer resp.Body.Close()
-
-	var countries []country
-	err = decodeResponse(resp, &countries)
-	if err != nil {
-		return nil, err
-	}
-
-	return countries, nil
-}
-
-func (c *Client) doAuthorized(req *http.Request) (*http.Response, error) {
-	if c.authToken.AccessToken == "" {
-		return nil, errors.New("please login before using authorized endpoints")
-	}
-	if !c.authToken.ExpiresAt.After(time.Now()) {
-		authToken, err := c.refresh(req.Context(), c.authToken)
+func (c *Client) doClientAuth(ctx context.Context, req *http.Request, v any) error {
+	if c.state.ClientToken.AccessToken == "" || time.Now().After(c.state.ClientToken.ExpiresAt) {
+		var err error
+		c.state.ClientToken, err = c.clientCredentials(ctx)
 		if err != nil {
-			return nil, errors.New("auth token expired: refresh failed")
+			return fmt.Errorf("client token: %w", err)
 		}
-		c.authToken = authToken
 	}
 
-	req.Header.Add("Authorization", c.authToken.Authorization())
+	req.Header.Add("Authorization", c.state.ClientToken.Authorization())
 
+	return c.do(ctx, req, v)
+}
+
+func (c *Client) doUserAuth(ctx context.Context, req *http.Request, v any) error {
+	if c.state.UserToken.AccessToken == "" {
+		return errors.New("please login before using this endpoint")
+	}
+	if time.Now().After(c.state.UserToken.ExpiresAt) {
+		authToken, err := c.refreshToken(ctx, c.state.UserToken)
+		if err != nil {
+			return fmt.Errorf("auth token expired: refresh failed: %w", err)
+		}
+		c.state.UserToken = authToken
+	}
+
+	req.Header.Add("Authorization", c.state.UserToken.Authorization())
+
+	return c.do(ctx, req, v)
+}
+
+func (c *Client) do(ctx context.Context, req *http.Request, v any) error {
 	resp, err := c.client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("do request: %w", err)
-	}
-
-	return resp, nil
-}
-
-// Appliances contains data from all appliances.
-func (c *Client) Appliances(ctx context.Context, includeMetadata bool) ([]Appliance, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/appliance/api/v2/appliances?includeMetadata=%t", c.regionalBaseURL, includeMetadata), nil)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := c.doAuthorized(req)
-	if err != nil {
-		return nil, fmt.Errorf("do authorized: %w", err)
+		return fmt.Errorf("http client do: %w", err)
 	}
 	defer resp.Body.Close()
 
-	var appliances []Appliance
-	err = decodeResponse(resp, &appliances)
-	if err != nil {
-		return nil, err
+	if resp.StatusCode != http.StatusOK {
+		b, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("unexpected status code for %q: %d, body: %s", resp.Request.URL.Path, resp.StatusCode, string(b))
 	}
 
-	return appliances, nil
-}
-
-// AppliancesInfo contains information about the requested appliances.
-func (c *Client) AppliancesInfo(ctx context.Context, applianceIDs ...string) ([]ApplianceInfo, error) {
-	body, err := json.Marshal(map[string][]string{
-		"applianceIds": applianceIDs,
-	})
+	err = json.NewDecoder(resp.Body).Decode(v)
 	if err != nil {
-		return nil, fmt.Errorf("marshal: %w", err)
-	}
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("%s/appliance/api/v2/appliances/info", c.regionalBaseURL), bytes.NewReader(body))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("Content-Type", "application/json")
-
-	resp, err := c.doAuthorized(req)
-	if err != nil {
-		return nil, fmt.Errorf("do authorized: %w", err)
-	}
-	defer resp.Body.Close()
-
-	var applianceInfo []ApplianceInfo
-	err = decodeResponse(resp, &applianceInfo)
-	if err != nil {
-		return nil, err
+		return fmt.Errorf("decode response: %w", err)
 	}
 
-	return applianceInfo, nil
+	return nil
 }
 
 type clientTransport struct {
@@ -325,22 +402,12 @@ func newClientTransport(rt http.RoundTripper, apiKey string) http.RoundTripper {
 func (ct *clientTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Add("x-api-key", ct.apiKey)
 	req.Header.Add("User-Agent", "Ktor client")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Accept-Charset", "UTF-8")
+	req.Header.Add("Accept-Language", "en-US,en;q=0.9")
+
+	// Go's http transport automatically requests gzip.
+	// req.Header.Add("Accept-Encoding", "gzip, deflate, br")
 
 	return ct.rt.RoundTrip(req)
-}
-
-func decodeResponse(resp *http.Response, v interface{}) error {
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		b, _ := io.ReadAll(resp.Body)
-		return fmt.Errorf("unexpected status code for %q: %d, body: %s", resp.Request.URL.Path, resp.StatusCode, string(b))
-	}
-
-	err := json.NewDecoder(resp.Body).Decode(v)
-	if err != nil {
-		return fmt.Errorf("decode response: %w", err)
-	}
-
-	return nil
 }
